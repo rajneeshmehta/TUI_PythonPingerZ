2069c3eb31f60ff59f835066d0f014d12fa34783
rajneeshmehta <44520236+rajneeshmehta@users.noreply.github.com>
2020-03-24 01:22:33 +0530

    Create menu.py
    I've written Day 1 code by Vimal Sir 
    I want from you Guys to make and keep it updated

---
+++ menu.py
@@ -0,0 +1,15 @@
+import os
+os.system("tputsetaf 1")
+print("""\t\t\tWelcome to Our TUI
+				Created by PythonPingerZ""")
+os.system("tput setaf 7")
+print(\t\t\t-------------------------------------------------------")
+print("""Press 1: to see Date
+Press 2 :to check cal
+Press 3 : conf web server
+Press 4 :to ceate user
+Press 5 :create file
+Press 6 :to setup NetWork
+Press 7 :to exit""")
+choice = int(input("Enter your choice : "))
+print(ch)
