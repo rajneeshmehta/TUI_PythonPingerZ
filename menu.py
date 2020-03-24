1a02718c405dff688f88a24850c84a9e70e796fd
Rajneesh Mehta <rajneeshmehta93@gmail.com>
2020-03-24 20:45:23 +0530

    Remote connecction feature added to menu.py

--- menu.py
+++ menu.py
@@ -14,18 +14,36 @@ Press 5 :create file
 Press 6 :to setup NetWork
 Press 7 :to exit""")
 choice = int(input("Enter your choice : "))
-if choice == 1:
-    os.system("date")
-elif choice == 2:
-    os.system("cal")
-elif choice == 3:
-    os.system("")
-elif choice == 4:
-    os.system("")
-elif choice == 5:
-    os.system("")
-elif choice == 6:
-    os.system("")
-elif choice == 7:
-    os.system("")
+location = input("Enter where you want to run command: ")
+if location == "local":
+    if choice == 1:
+        os.system("date")
+    elif choice == 2:
+        os.system("cal")
+    elif choice == 3:
+        os.system("")
+    elif choice == 4:
+        os.system("")
+    elif choice == 5:
+        os.system("")
+    elif choice == 6:
+        os.system("")
+    elif choice == 7:
+        os.system("")
+elif location == remote:
+    Server_IP = input("Enter the server IP: ")
+    if choice == 1:
+        os.system("ssh {} date".format(Server_IP))
+    elif choice == 2:
+        os.system("ssh {} cal".format(Server_IP))
+    elif choice == 3:
+        os.system("")
+    elif choice == 4:
+        os.system("")
+    elif choice == 5:
+        os.system("")
+    elif choice == 6:
+        os.system("")
+    elif choice == 7:
+        os.system("")
 
