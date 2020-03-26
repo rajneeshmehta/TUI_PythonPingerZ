5ff87e69326411e978dcf1a52dc3dcb443ba3d2e
Rajneesh Mehta <rajneeshmehta93@gmail.com>
2020-03-26 15:35:45 +0530

    Code cleaned use of fuctions included
    Gui Enhanced

--- menu.py
+++ menu.py
@@ -1,49 +1,28 @@
 #these variable acts as switch for changing the color
-global_background = 3
-welcome_text = 4
-welcome_background = 10
-created_text = 17
-created_background = 14
-location_text = 9
-location_background = 15
-options_text = 4
-options_background = 11
-choice_text = 17
-choice_background = 15
-exit_text = 15
-exit_background = 0
-server_ip_text = 4
-server_ip_background = 10
-continue_text = 4
-continue_background = 13
-# import necessory module so we can run cmd through python
-import os
-os.system("tput setab {}".format(global_background))
-
-os.system("clear")
-
-os.system("tput setaf {}".format(welcome_text))
-os.system("tput setab {}".format(welcome_background))
-
-print("\t\t\t\tWelcome to Our TUI")
-
-os.system("tput setaf {}".format(created_text))
-os.system("tput setab {}".format(created_background))
-print("\t\t\t     Created by PythonPingerZ     ")
+# import out user define function as well as variables
+from Function import *
+Message()
 
 os.system("tput setaf {}".format(location_text))
 os.system("tput setab {}".format(location_background))
-location = input("Enter where you want to run command: ")
+location = input("\nEnter where you want to run command: ")
 
 if location == "remote":
     os.system("tput setaf {}".format(server_ip_text))
     os.system("tput setab {}".format(server_ip_background))
     Server_IP = input("Enter the server IP: ")
+    print("\nChecking connection......")
+    os.system("sleep 2s")
+
+#We have to add a connection checking code here
 
 
 choice = 0  #let 0 be the default
 
 while choice != 7:
+    Message()
+    
+    
     os.system("tput setaf {}".format(options_text))
     os.system("tput setab {}".format(options_background))
 
@@ -93,7 +72,10 @@ Press 7 :to exit         """)
         elif choice == 6:
             os.system("")
         elif choice == 7:
-            os.system("")
+            os.system("tput setaf {}".format(exit_text))#before exiting make text white
+            os.system("tput setab {}".format(exit_background))#and background black
+            os.system("clear")#clear the screen
+            exit()
             
     os.system("tput setaf {}".format(continue_text))
     os.system("tput setab {}".format(continue_background))
@@ -102,6 +84,3 @@ Press 7 :to exit         """)
 
     os.system("clear")
     
-
-
-
