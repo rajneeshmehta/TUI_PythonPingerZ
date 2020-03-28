79c73c28af1a57fd88680868c88ce7aa800953db
Rajneesh_Mehta <rajneeshmehta93@gmail.com>
2020-03-28 19:12:40 +0530

    Updated Comment this is kind of neccsary to commit somthing... it makes easier to know what exactly has been updated notice > sign in left..... it means whatevev I'm typing is going to uploaded to github

--- menu.py
+++ menu.py
@@ -83,4 +83,4 @@ Press 7 :to exit         """)
     os.system("tput setab {}".format(global_background))
 
     os.system("clear")
-#lets say I made some changes here     
+#lets say we have made some random changes in this file even that is comment     
