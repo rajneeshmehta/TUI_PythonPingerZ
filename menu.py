c76360fdf1fd76bd56a4b415fb35fd2e04ca798a
kartikprajapat <kr.pjpt@gmail.com>
2020-03-29 01:01:18 -0400

    Added some more option 28 March 2020 1. Added local or remote. 2. Added create user option added. 3. Added create file 4. Changed exit value from 7 to 0. So that when adding more option it would not    require to change its value again and again. 5. Added option to check if a program is installed or not

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ Message()
 
 os.system("tput setaf {}".format(location_text))
 os.system("tput setab {}".format(location_background))
-location = input("\nEnter where you want to run command: ")
+location = input("\nEnter where you want to run command(local or remote): ")
 
 if location == "remote":
     os.system("tput setaf {}".format(server_ip_text))
@@ -17,23 +17,25 @@ if location == "remote":
 #We have to add a connection checking code here
 
 
-choice = 0  #let 0 be the default
+choice = -1  #let -1 be the default
 
-while choice != 7:
+while choice != 0:
     Message()
-    
-    
+
+
     os.system("tput setaf {}".format(options_text))
     os.system("tput setab {}".format(options_background))
 
     print("""
-Press 1: to see Date     
-Press 2 :to check cal    
-Press 3 :conf web server 
-Press 4 :to ceate user   
-Press 5 :create file     
-Press 6 :to setup NetWork
-Press 7 :to exit         """)
+Press 1: to see Date
+Press 2 :to check cal
+Press 3 :conf web server*
+Press 4 :to ceate user
+Press 5 :create file
+Press 6 :to setup NetWork*
+Press 7 :to check program is installed or not
+Press 0 :to exit
+*These files are not completed.    """)
     os.system("tput setaf {}".format(choice_text))
     os.system("tput setab {}".format(choice_background))
     choice = int(input("Enter your choice : "))
@@ -45,20 +47,24 @@ Press 7 :to exit         """)
         elif choice == 3:
             os.system("")
         elif choice == 4:
-            os.system("")
+            name = str(input("Enter the name of user you want to add: "))
+            os.system("useradd {}".format(name))
         elif choice == 5:
-            os.system("")
+            filename = str(input("Please enter the name of file with its format: "))
+            os.system("touch {}".format(filename))
+            os.system("echo \"your file has been created\"")
         elif choice == 6:
             os.system("")
         elif choice == 7:
-
+            programName = str(input("Please enter the program name you want to check if it is installed or not: "))
+            os.system("rpm -q {}".format(programName))
+        elif choice == 0:
             os.system("tput setaf {}".format(exit_text))#before exiting make text white
             os.system("tput setab {}".format(exit_background))#and background black
             os.system("clear")#clear the screen
             exit()
 
     elif location == "remote":
-        
         if choice == 1:
             os.system("ssh {} date".format(Server_IP))
         elif choice == 2:
@@ -66,21 +72,26 @@ Press 7 :to exit         """)
         elif choice == 3:
             os.system("")
         elif choice == 4:
-            os.system("")
+            name = str(input("Enter the name of user you want to add: "))
+            os.system("ssh {} useradd {}".format(Server_IP,name))
         elif choice == 5:
-            os.system("")
+            filename = str(input("Please enter the name of file with its format: "))
+            os.system("ssh {} touch {}".format(Server_IP,filename))
+            os.system("ssh {} echo \"your file has been created\"".format(Server_IP))
         elif choice == 6:
             os.system("")
         elif choice == 7:
+            programName = str(input("Please enter the program name you want to check if it is installed or not: "))
+            os.system("ssh {} rpm -q {}".format(Server_IP,programName))
             os.system("tput setaf {}".format(exit_text))#before exiting make text white
             os.system("tput setab {}".format(exit_background))#and background black
             os.system("clear")#clear the screen
             exit()
-            
+
     os.system("tput setaf {}".format(continue_text))
     os.system("tput setab {}".format(continue_background))
     input("Enter to continue...")
     os.system("tput setab {}".format(global_background))
 
     os.system("clear")
-#I've changed comment     
+#I've changed comment
